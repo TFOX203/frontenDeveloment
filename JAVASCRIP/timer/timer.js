089294d7073c42b82cf7e232b6217f2f9f27a608
TFOX203 <pesasimeran87@gmail.com>
2026-02-11 12:59:57 +0100

    timer bucle

--- JAVASCRIP/timer/timer.js
+++ JAVASCRIP/timer/timer.js
@@ -1,10 +1,34 @@
-//"document" object represent the whole html page wich is a tree data structure
-//document.getElementById("id of an element in the html page")
-//this mothod or command allows is to retrive an element in our page by its id
+//"document" object represents the whole html page wich is a tree data structure
+//document.getElementById("timerDisplay")
+//this method or command allows us to retrieve an element in our page by its id
 
 const timerDisplay = document.getElementById("timerDisplay");
 console.debug(timerDisplay);
+const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
+const resetBtn = document.getElementById("resetBtn");
+console.log(startBtn);
+console.log(stopBtn);
+console.log(resetBtn);
+//change the font size of the timer label using js
+timerDisplay.style.fontSize = "40px";
 
-const start = document.getElementById("start");
-const stop = document.getElementById("stop");
-const reset = document.getElementById("reset");
+startBtn.addEventListener("click", ()=>{console.log("you are clicking me")});
+sizeAnimation();
+
+function sizeAnimation(){
+let size = 40;
+let x = 1000;
+while(x == 0){
+//increment the font size
+for (let i = 0; i < 50; i++){
+    size++;
+    timerDisplay.style.fontSize = size + "px";
+}
+for (let i = 0; i < 50; i++){
+    size--;
+    timerDisplay.style.fontSize = size + "px";
+}
+    x--;
+}
+}
