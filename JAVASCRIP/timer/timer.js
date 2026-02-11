67bdd180f2ff0d543f6b825d0ffa99a6369f9782
TFOX203 <pesasimeran87@gmail.com>
2026-02-11 11:32:58 +0100

    timer

---
+++ JAVASCRIP/timer/timer.js
@@ -0,0 +1,5 @@
+//"document" object represent the whole html page wich is a tree data structure
+//document.getElementById("id of an element in the html page")
+//this mothod or command allows is to retrive an element in our page by its id
+const timerDisplay = document.getElementById("timerDisplay");
+console.debug(timerDisplay);
