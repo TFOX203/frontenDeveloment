9639cbf3721285553709bca3623c30f961859028
TFOX203 <pesasimeran87@gmail.com>
2026-02-11 11:38:26 +0100

    timer

--- JAVASCRIP/timer/timer.js
+++ JAVASCRIP/timer/timer.js
@@ -1,5 +1,10 @@
 //"document" object represent the whole html page wich is a tree data structure
 //document.getElementById("id of an element in the html page")
 //this mothod or command allows is to retrive an element in our page by its id
+
 const timerDisplay = document.getElementById("timerDisplay");
 console.debug(timerDisplay);
+
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
